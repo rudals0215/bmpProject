9d54bfe7bfe046fd8a525f9243cbdb7414fdf415
cinxdy <cinxdys2@gmail.com>
2021-02-06 08:21:20 +0000

    Updated

--- bmpLoad.c
+++ bmpLoad.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 #define BUF_MAX 100
+#define THR_SIZE 4
 
 typedef struct __attribute__((__packed__)) {                                                                                                                                                                                                                             
     unsigned char fileMarker1;                                                                                                                                                                                              
@@ -34,6 +35,9 @@ typedef struct __attribute__((__packed__)) {
 } IMAGE;
 
 
+void divideSection(int index[][4], int width, int height, int numSection);
+void* func(void *arg);
+
 int main(){
     char buf[BUF_MAX];
                                                                                                                                                                                                                              
@@ -50,15 +54,56 @@ int main(){
     IMAGE img[ih.width][ih.height];
     fread(img, sizeof(unsigned char), sizeof(IMAGE)*ih.width*ih.height, fp);
 
+    printf("### Color Palette ###\n");
     for(int x=0;x<ih.width;x++){
         for(int y=0;y<ih.height;y++){
-            // if(x<3 && y<3)
+            if(x<3 && y<3)
             printf("img[%d,%d] BGR : %u %u %u\n",x,y,img[x][y].b,img[x][y].g,img[x][y].r);
         }
     }
-    // while(!feof(fp)){
-    //     fread(buf, sizeof(char), BUF_MAX, fp);
-    //     printf("%s\n",buf);
-    // }
+
+	pthread_t t_id[THR_SIZE];
+
+    int index[THR_SIZE][4];
+    divideSection(index, ih.width, ih.height, THR_SIZE);
+    for(int i=0;i<THR_SIZE;i++){
+        printf("pthread\n");
+	    if (pthread_create(&t_id[i], NULL, func, (void**)&index[i]) != 0){
+            puts("pthread_create() error");
+            return -1;
+	    }
+	    if(pthread_join(t_id[i], NULL)!=0){
+            puts("pthread_join() error");
+            return -1;
+        }
+        printf("pthread\n");
+    }
+
     return 0;
+}
+
+void* func(void *arg){
+    int startX = ((int*)arg)[0];
+    int startY = ((int*)arg)[1];
+	int endX = ((int*)arg)[2];
+    int endY = ((int*)arg)[3];
+
+    for(int x=startX; x<=endX; x++)
+        for(int y=startY; y<=endY; y++)
+            printf("change Color\n");
+    
+	return NULL;
+}
+
+void divideSection(int index[][4], int width, int height, int numSection){
+    printf("divide Section %d\n", numSection);
+    printf("width X height %d X %d\n", width, height);
+    for(int i=0;i<numSection;i++){
+        index[i][0] = width / THR_SIZE * i;
+        index[i][1] = height;
+        index[i][2] = width / THR_SIZE * (i + 1);
+        index[i][3] = height;
+
+        printf("Section %d: [%d,%d]-> [%d,%d]\n", i, index[i][0],index[i][1],index[i][2],index[i][3]);
+    }    
 }
